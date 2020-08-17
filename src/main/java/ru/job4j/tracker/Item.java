71e9e9182db525718c2ca7fb4e1d977e1e3b299d
AlexeyEsipov <a_esipov@list.ru>
2020-08-17 22:14:30 +0400

    0. Сортировка [#314049]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
-public class Item {
+import java.util.Objects;
+
+public class Item implements Comparable<Item> {
 
     private int id;
     private String name;
@@ -26,5 +28,35 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int compareTo(Item another) {
+        return Integer.compare(id, another.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + " ID= " + id + ","
+                + " name='" + name + '\''
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
