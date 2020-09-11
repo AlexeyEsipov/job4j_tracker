d8f5ff7f89dd5666d974b8ce9bb12de366ac0a55
AlexeyEsipov <a_esipov@list.ru>
2020-09-11 19:03:37 +0300

    3. Ссылки на методы [#313888]

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -1,17 +1,24 @@
 package ru.job4j.lambda;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class FI {
 
     public static void main(String[] args) {
-        Attachment[] atts = {
-                new Attachment("image 1", 20),
+                Attachment[] atts = {
+                new Attachment("imagebug 1", 20),
                 new Attachment("image  3", 120),
                 new Attachment("image2", 23)
         };
         System.out.println(Arrays.toString(atts));
+        List<Attachment> listAtts = Arrays.asList(atts);
+
+        System.out.println(SearchAtt.filterName(listAtts));
+        System.out.println(SearchAtt.filterSize(listAtts));
+        System.out.println("--------");
         Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
         Comparator<Attachment> cmpSize = (left, right) -> left.getName().length() - right.getName().length();
         Comparator<Attachment> cmpDescSize = (left, right) -> {
