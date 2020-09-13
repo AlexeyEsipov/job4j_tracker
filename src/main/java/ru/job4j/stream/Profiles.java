6cd8e3b0179645e549a5e8dfd296d58c10810267
AlexeyEsipov <a_esipov@list.ru>
2020-09-13 09:36:51 +0300

    3. Уникальность элементов и сортировка. [#313902]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,17 +1,22 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    private List<Profile> listProfile = new ArrayList<>();
+    private final List<Profile> listProfile = new ArrayList<>();
 
     public void add(Profile profile) {
         this.listProfile.add(profile);
     }
 
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -14,12 +14,12 @@ public class ProfilesTest {
     public void collectAddress() {
         Profiles profiles = new Profiles();
         List<Profile> listProfile = new ArrayList<>();
-        Address address1 = new Address("city", "street", 100, 10);
+        Address address1 = new Address("city2", "street2", 200, 20);
         listProfile.add(new Profile("Ivan", address1));
-        Address address2 = new Address("city2", "street2", 200, 20);
-        listProfile.add(new Profile("Ivan", address2));
+        Address address2 = new Address("city1", "street1", 100, 10);
+        listProfile.add(new Profile("Alex", address2));
         List<Address> addresses = profiles.collect(listProfile);
-        assertThat(addresses.get(0).getStreet(), is("street"));
-        assertEquals(addresses.get(1), address2);
+        assertThat(addresses.get(0).getCity(), is("city1"));
+        assertEquals(addresses.get(1), address1);
     }
 }
