2168abcc2a9ab69536b831864b80525b77e2aa5d
AlexeyEsipov <a_esipov@list.ru>
2021-01-01 20:18:56 +0300

    1. Sql схема для проекта Tracker. [#379831]

--- src/main/java/ru/job4j/tracker/db/create.sql
+++ src/main/java/ru/job4j/tracker/db/create.sql
@@ -1,3 +1,4 @@
+
 create table if not exists items(
   id serial primary key,
   name text
--- src/main/java/ru/job4j/tracker/db/insert.sql
+++ src/main/java/ru/job4j/tracker/db/insert.sql
@@ -2,7 +2,6 @@ create  table if not exists items(
                       id serial primary key,
                       name text
 );
-
 insert into items (name) values ('text1'), ('text2'), ('text3');
 
 
